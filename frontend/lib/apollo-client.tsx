096e869adf0cb7194459c02d0c4a44c5b8c10a70
yourmentor-08 <saumyasinghkrvp@gmail.com>
2025-01-28 13:08:36 +0530

    Updated remote service url

--- frontend/lib/apollo-client.tsx
+++ frontend/lib/apollo-client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://93a8-106-219-174-199.ngrok-free.app', 
+  uri: 'https://nextread-production.up.railway.app/', 
   cache: new InMemoryCache(),
 });
 
